21e7ef185c24eb167c915015d7b7db925bc69d6a
jason staerck <jason.staerck@moneysavingexpert.com>
2017-09-11 09:43:14 +0100

    added cors

--- server.js
+++ server.js
@@ -5,11 +5,25 @@ mongoose = require('mongoose'),
 Task = require('./models/model'), //created model loading here
 bodyParser = require('body-parser');
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+
 // mongoose instance connection url connection
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://jason:Monty2013@ds129394.mlab.com:29394/heroku_3c5gfdnk'); 
 
-
+app.use(allowCrossDomain);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
